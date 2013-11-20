bd2caf4bef392c16e1088c5361132d92e76c2244
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2013-11-20 07:28:14 +0100

    done !

--- HW4/student_func.cu
+++ HW4/student_func.cu
@@ -41,6 +41,42 @@
    at the end.
 
  */
+void print_cuda_array(unsigned int* d_data, size_t size){
+  unsigned int *h_data;
+  h_data = (unsigned int *)malloc(sizeof(unsigned int)*size);
+  cudaMemcpy(h_data, d_data, sizeof(unsigned int) * size, cudaMemcpyDeviceToHost);
+  for (int i=0; i<size; i++){
+      printf("%d ",h_data[i]);
+  }
+  printf("\n");
+  free(h_data);
+}
+
+void sum_array(unsigned int* d_data, size_t size){
+  unsigned int *h_data, sum=0;
+  h_data = (unsigned int *)malloc(sizeof(unsigned int)*size);
+  cudaMemcpy(h_data, d_data, sizeof(unsigned int) * size, cudaMemcpyDeviceToHost);
+  for (int i=0; i<size; i++){
+      sum+=h_data[i];
+  }
+  printf("Summed items: %d\n", sum);
+  free(h_data);
+}
+void save_array(unsigned int* d_data, size_t size, char* prefix, int i){
+  FILE *filePtr;
+  char *name;
+  name = (char *)malloc(80);
+  sprintf(name,"%s-%02d.txt",prefix,i);
+  filePtr = fopen(name,"w");
+  unsigned int *h_data, sum=0;
+  h_data = (unsigned int *)malloc(sizeof(unsigned int)*size);
+  cudaMemcpy(h_data, d_data, sizeof(unsigned int) * size, cudaMemcpyDeviceToHost);
+   for (int i = 0; i < size; i++) {
+      fprintf(filePtr, "%d ", h_data[i]);
+   }
+   free(h_data);
+   free(name);
+}
 __global__
 void blelloch1( unsigned int* data,
 				unsigned int step,
@@ -90,9 +126,10 @@ unsigned int cumsum(unsigned int *d_array,
 
 	unsigned int size = (1<<dmax),
 		  	  sum=0;
-
-  for (unsigned int d=0; d<(dmax-1); d++){
-    printf( "CUDA blelloch1 d= %d/%d (%d)\n",d,dmax,(1<<(d+1)));
+//		  	  *h_array;
+//  h_array = (unsigned int *)malloc(sizeof(unsigned int)*size);
+  for (unsigned int d=0; d<(dmax); d++){
+//    printf( "CUDA blelloch1 d= %d/%d (%d)\n",d,dmax,(1<<(d+1)));
     blelloch1<<<size/WORKGROUP_SIZE, WORKGROUP_SIZE>>>(d_array, (1<<(d+1)), size);
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
   }
@@ -100,12 +137,15 @@ unsigned int cumsum(unsigned int *d_array,
   //One should retrieve the sum of all data here
 
   cudaMemcpy(&sum, &d_array[size-1], 1 * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+//  printf( "CUDA sum is %d\n", sum);
+//  for (int i=0; i<size; i++) printf("%d ",h_array[i]); printf("\n");
+
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
   blelloch2<<<1,1>>>(d_array, 1, size);
   blelloch3<<<1,1>>>(d_array, 1, size);
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
   for (int d=dmax-2;d>=0; d--){
-    printf( "CUDA blelloch2 d= %d/%d (%d)\n",d,dmax,1<<(d+1));
+//    printf( "CUDA blelloch2 d= %d/%d (%d)\n",d,dmax,1<<(d+1));
     blelloch4<<<size/WORKGROUP_SIZE,WORKGROUP_SIZE>>>(d_array, 1<<(d+1), size);
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
@@ -127,8 +167,8 @@ void calc_predicate(unsigned int* data,
       index0[idx] = 0;
     }
     else{
-      index1[idx] = 1;
-      index0[idx] = 0;
+      index1[idx] = 0;
+      index0[idx] = 1;
     }
 
   }
@@ -154,7 +194,7 @@ void reorder(unsigned int* inputVals,
 		   	    pos = inputPos[idx];
 //		   	    offset = index0[numElems-1]; //todo check
 
-    if (value &(1<<bitPos) ==0){
+    if ((value &(1<<bitPos))==0){
       new_idx = index0[idx];
     }
     else{
@@ -177,27 +217,40 @@ void your_sort(unsigned int* const d_inputVals,
 		  	   sum0 = 0,
 		  	   sum1 = 0;
 
-  printf("Size of the system: %d padded to %d",numElems, padded_size);
+//  printf("Size of the system: %d padded to %d\n",numElems, padded_size);
   //Malloc stuff
   unsigned int *d_index0,*d_index1;
   checkCudaErrors(cudaMalloc(&d_index0, (size_t)  padded_size*sizeof(unsigned int)));
   checkCudaErrors(cudaMalloc(&d_index1, (size_t)  padded_size*sizeof(unsigned int)));
-
   // do some work
-  for (int i=0;i<1;i++){
+  for (int i=0;i<8*sizeof(unsigned int);i++){
+//    print_cuda_array(d_inputVals,40);
+//    save_array(d_inputVals,numElems,"data",i);
     calc_predicate<<<padded_size/WORKGROUP_SIZE,WORKGROUP_SIZE>>>(d_inputVals,d_index0,d_index1,i,numElems);
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+//    sum_array(d_index0,padded_size);
+//    sum_array(d_index1,padded_size);
+//    print_cuda_array(d_index0,40);
+//    print_cuda_array(d_index1,40);
+//    save_array(d_index0,numElems,"predicate0",i);
     sum0 = cumsum(d_index0,dmax);
+//    save_array(d_index0,numElems,"cumsum0",i);
     if ((sum0==0)||(sum0==numElems))
     	continue;
-    printf("Found %d items at zero for bit #%d",sum0,i);
+//    printf("Found %d items at zero for bit #%d\n",sum0,i);
     sum1 = cumsum(d_index1,dmax);
-    printf("Found %d items at one  for bit #%d",sum1,i);
+//    printf("Found %d items at one  for bit #%d\n",sum1,i);
     reorder<<<padded_size/WORKGROUP_SIZE,WORKGROUP_SIZE>>>(d_inputVals, d_inputPos, d_outputVals, d_outputPos,
     		d_index0, d_index1, i, numElems, sum0);
+    cudaMemcpy(d_inputVals, d_outputVals, numElems * sizeof(unsigned int), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(d_inputPos, d_outputPos, numElems * sizeof(unsigned int), cudaMemcpyDeviceToDevice);
+//    print_cuda_array(d_outputVals,40);
+
   }
+//  print_cuda_array(d_outputVals,numElems);
   //Free stuff
   checkCudaErrors(cudaFree(d_index0));
   checkCudaErrors(cudaFree(d_index1));
+//  save_array(d_outputVals,numElems,"data",32);
 
 }
