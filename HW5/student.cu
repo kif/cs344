fa8ed321097a9cfab4a954193669cb98d62a3476
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2013-11-27 21:54:56 +0100

    not working ... to share

--- HW5/student.cu
+++ HW5/student.cu
@@ -24,7 +24,7 @@
 
 */
 
-
+#include <stdio.h>
 #include "utils.h"
 
 __global__
@@ -32,12 +32,91 @@ void yourHisto(const unsigned int* const vals, //INPUT
                unsigned int* const histo,      //OUPUT
                int numVals)
 {
+  // Optimized for WG=8, treat 2048 vals per WG
+  // each thread processes 256 val
+  // unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int start = blockIdx.x*2048;
+  __shared__ unsigned char lhist[10000];
+  for (unsigned int i=0;i<1024;i++){
+    unsigned int proc_bin = i + 1024*threadIdx.x;
+    lhist[proc_bin] = 0;
+  }
+  for (unsigned char i=0;i<256;i++){
+    unsigned int pos = start+i*8+threadIdx.x;
+    if (pos<numVals){
+      unsigned int val = vals[pos];
+      unsigned int proc_bin = 1024*threadIdx.x + val;
+      lhist[proc_bin]+=1;
+    }
+  }
+  __syncthreads();
+  //parallel reduce
+  for (unsigned int i=0; i<1024; i+=8){
+    unsigned int sum = (unsigned int)(lhist[i+threadIdx.x]) +
+        lhist[i+threadIdx.x+1024] +
+        lhist[i+threadIdx.x+2048] +
+        lhist[i+threadIdx.x+3072] +
+        lhist[i+threadIdx.x+4096] +
+        lhist[i+threadIdx.x+5120] +
+        lhist[i+threadIdx.x+6144] +
+        lhist[i+threadIdx.x+7168];
+        atomicAdd(&histo[i+threadIdx.x],sum);
+  }
+
+}
+
+
+__global__
+void nogood1Histo(const unsigned int* const vals, //INPUT
+               unsigned int* const histo,      //OUPUT
+               int numVals)
+{
+  //Optimized for WG=32, treat 2048 vals per WG
   //TODO fill in this kernel to calculate the histogram
   //as quickly as possible
+//  unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int start = blockIdx.x*2048;
+  __shared__ unsigned int lvals[2048];
+  __shared__ unsigned int lhist[1024];
+  for (int i=0;i<64;i++){
+    unsigned int j = i*32 + threadIdx.x;
+    if (j+start<numVals)
+      lvals[j] = vals[start+j];
+    else
+      lvals[j] = 0;
 
-  //Although we provide only one kernel skeleton,
-  //feel free to use more if it will help you
-  //write faster code
+  }
+  for (unsigned int i=0;i<32;i++){
+    unsigned int proc_bin = i*32 + threadIdx.x;
+    lhist[proc_bin] = 0;
+  }
+  __syncthreads();
+
+  for (unsigned int i=0;i<32;i++){
+    unsigned int proc_bin = i*32 + threadIdx.x;
+    for (unsigned int j=0;j<2048;j++){
+      if (lvals[j] == proc_bin)
+        lhist[proc_bin]+=1;
+    }
+  }
+  __syncthreads();
+  for (unsigned int i=0;i<32;i++){
+    unsigned int proc_bin = i*32 + threadIdx.x;
+    atomicAdd(&histo[proc_bin],lhist[proc_bin]);
+  }
+
+}
+
+__global__
+void atomicHisto(const unsigned int* const vals, //INPUT
+               unsigned int* const histo,      //OUPUT
+               int numVals)
+{
+  unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx>=numVals)
+    return;
+  unsigned int value = vals[idx];
+  atomicAdd(&histo[value],1);
 }
 
 void computeHistogram(const unsigned int* const d_vals, //INPUT
@@ -46,9 +125,11 @@ void computeHistogram(const unsigned int* const d_vals, //INPUT
                       const unsigned int numElems)
 {
   //TODO Launch the yourHisto kernel
-
-  //if you want to use/launch more than one kernel,
-  //feel free
-
+  printf("OK we are working on an input array of %d element and histogramming into %d bins\n",numElems,numBins);
+  int BLOCK_SIZE=8;
+  int PROC_PER_BLOCK=2048;
+  dim3 num_blocks((numElems+PROC_PER_BLOCK-1)/PROC_PER_BLOCK);
+  dim3 block_size(BLOCK_SIZE);
+  yourHisto<<<num_blocks,block_size>>>(d_vals, d_histo, numElems);
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 }
